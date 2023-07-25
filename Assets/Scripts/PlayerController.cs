5f01efcbb3a7919301b15ea4aa05c784b41108ff
azarg <azar.gurbanov@gmail.com>
2023-07-25 18:20:50 -0500

    added spawn delay

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     private GameObject currentBall;
     [SerializeField] private float force;
     [SerializeField] private float forceAngle;
+    [SerializeField] private float spawnDelay;
+
     private bool mouseClicked;
 
     // Start is called before the first frame update
@@ -28,14 +30,28 @@ public class PlayerController : MonoBehaviour
     {
         if(mouseClicked)
         {
+            mouseClicked = false;
             if (currentBall != null)
             {
-                var rb = currentBall.GetComponent<Rigidbody>();
-                rb.useGravity = true;
-                rb.AddForce(Quaternion.Euler(forceAngle, 0, 0) * Vector3.forward * force, ForceMode.Impulse);
-                mouseClicked = false;
-                currentBall = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+                Kick(currentBall);
+
+                // set to null so that further mouse clicks do not kick the same ball
+                currentBall = null;
+                StartCoroutine(SpawnNextBall());
             }
         }
+    }
+
+    private void Kick(GameObject ball)
+    {
+        var rb = ball.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.AddForce(Quaternion.Euler(forceAngle, 0, 0) * Vector3.forward * force, ForceMode.Impulse);
+    }
+
+    IEnumerator SpawnNextBall()
+    {
+        yield return new WaitForSeconds(spawnDelay);
+        currentBall = Instantiate(ballPrefab, transform.position, Quaternion.identity);
     }
 }
